2f56b9df9f43084ecb9332fc98ec01f2f189f45d
Margarita Vankevich <margaritavankevich@gmail.com>
2022-09-29 16:03:31 +0300

    Add project files.

---
+++ EventPLanningAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventPLanningAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Models.EventPlanning> EventPlannings { get; set; }
+    }
+}
